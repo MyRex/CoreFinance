b7963b3130eada3fbfe11ecc9ec74b8630d440fc
Mars <rex_bee@sina.com>
2018-09-27 15:02:46 +0800

    20180927-1

---
+++ ZFine.Data/DBContext/DbBackupEntity.cs
@@ -0,0 +1,23 @@
+﻿/*******************************************************************************
+ * Copyright © 2016
+ * 
+ * Description: MVC快速开发平台
+ *
+*********************************************************************************/
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZFine.Data.DBContext
+{
+    [Table("FilterIP")]
+    public class FilterIPEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public long Number { get; set; }
+        public string Name { get; set; }
+        public bool Sex { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
